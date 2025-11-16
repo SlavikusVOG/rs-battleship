1a7a867912e986716fe696f167d01cb65d7caf12
SlavikusVOG <slavaolish@gmail.com>
2025-11-16 12:09:52 +0300

    feat: add ws server with default config

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import WebSocket, { WebSocketServer } from 'ws';
+import http from 'node:http';
+import { error } from 'node:console';
+
+export class Server {
+  private readonly wsServer: WebSocketServer;
+  private readonly httpServer: http.Server;
+
+  constructor(httpServer: http.Server) {
+    this.httpServer = httpServer;
+
+    this.wsServer = new WebSocketServer({
+      server: httpServer,
+      port: 8080,
+      perMessageDeflate: {
+        zlibDeflateOptions: {
+          // See zlib defaults.
+          chunkSize: 1024,
+          memLevel: 7,
+          level: 3
+        },
+        zlibInflateOptions: {
+          chunkSize: 10 * 1024
+        },
+        // Other options settable:
+        clientNoContextTakeover: true, // Defaults to negotiated value.
+        serverNoContextTakeover: true, // Defaults to negotiated value.
+        serverMaxWindowBits: 10, // Defaults to negotiated value.
+        // Below options specified as default values.
+        concurrencyLimit: 10, // Limits zlib concurrency for perf.
+        threshold: 1024 // Size (in bytes) below which messages
+        // should not be compressed if context takeover is disabled.
+      }
+    });
+
+    this.setupEventHandlers();
+  }
+
+  setupEventHandlers () {
+    this.wsServer.on('connection', (/* ws, req */) => {
+      // TODO: implement
+    });
+
+    this.wsServer.on('error', (error) => {
+      console.error(`WS server error: ${error.message}`);
+    })
+
+    this.wsServer.on('close', () => {
+      console.log('ws server closed');
+    })
+  }
+}
